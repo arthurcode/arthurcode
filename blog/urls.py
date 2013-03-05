bff69c3144b4ed637b0534138824e2d78e80315f
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-03-05 13:54:30 -0700

    views: add About and Contact pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ __author__ = 'rhyanarthur'
 from django.conf.urls import patterns, url, include
 from blog.views import BlogYearArchiveView, BlogMonthArchiveView, BlogDayArchiveView, BlogArchiveView, \
     BlogPostDetailView, index
-
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
 
@@ -33,5 +33,7 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    (r'^comments/', include('django.contrib.comments.urls'))
+    (r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^about/', TemplateView.as_view(template_name="blog/about.html"), name="about"),
+    url(r'^contact/', TemplateView.as_view(template_name="blog/contact.html"), name="contact"),
 )
