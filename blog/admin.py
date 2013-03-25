ddd11a12b883cc4c2cda87a145b59d5fb90905f1
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-03-25 10:54:24 -0600

    admin: make Post.is_draft read-only
    The is_draft field should only be modified via the publish() routine
    which also takes care of resetting the pub_date and mod_date fields.

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,8 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'title_slug': ('title',)}
     date_hierarchy = 'pub_date'
 
+    readonly_fields = ('is_draft',)
+
 
 class AuthorProfileAdmin(admin.ModelAdmin):
     pass
