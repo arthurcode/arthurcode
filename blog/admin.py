389597d7edb32db39ba7d25baecef34293fd6369
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-03-25 13:25:44 -0600

    admin: add a 'Publish' button to the Post admin form
    As expected, pushing this button will publish the selected post.  The
    button is only displayed for posts where is_draft=True or in cases
    where the post does not yet exist in the database.

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from blog.models import Post, AuthorProfile
+from django.forms.models import ModelForm
+
+
+class PostAdminForm(ModelForm):
+    class Meta:
+        model = Post
+
+    def save(self, commit=True):
+        """
+        Handling for the custom 'Publish' admin button
+        """
+        instance = super(PostAdminForm, self).save(commit=commit)
+
+        if self.data.has_key('publish'):
+            instance.publish()
+        return instance
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'title_slug': ('title',)}
     date_hierarchy = 'pub_date'
-
     readonly_fields = ('is_draft',)
+    form = PostAdminForm
 
 
 class AuthorProfileAdmin(admin.ModelAdmin):
