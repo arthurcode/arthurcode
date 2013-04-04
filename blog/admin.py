0606a24c35d9bcb097c1d6aa5d9b7e85f2fcdbfb
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-04 15:20:28 -0600

    admin: beef up the Post admin list display
    I added some extra fields for display, along with some handy filters
    and search fields.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from blog.models import Post, AuthorProfile
 from django.forms.models import ModelForm
+from django.contrib.admin.filters import SimpleListFilter
 
 
 class PostAdminForm(ModelForm):
@@ -18,11 +19,28 @@ class PostAdminForm(ModelForm):
         return instance
 
 
+class AuthorFilter(SimpleListFilter):
+    title = "author"
+    parameter_name = "author"
+
+    def lookups(self, request, model_admin):
+        return [(a.id, a.pen_name) for a in AuthorProfile.objects.all()]
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        return queryset.filter(author__id=self.value())
+
+
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'title_slug': ('title',)}
     date_hierarchy = 'pub_date'
     readonly_fields = ('is_draft',)
     form = PostAdminForm
+    ordering = ('-pub_date',)
+    search_fields = ('title', 'body', 'synopsis')
+    list_display = ('title', 'author', 'pub_date', 'mod_date', 'is_draft', 'enable_comments')
+    list_filter = ('pub_date', 'mod_date', 'is_draft', AuthorFilter)
 
 
 class AuthorProfileAdmin(admin.ModelAdmin):
