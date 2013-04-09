73e47adb8b9906bc31e20e1a49bba0c068d2fcb8
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-09 10:03:40 -0600

    views: add a skeleton category view
    /products/<category-slug>

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from catalogue.views import product_detail_view
+from catalogue.views import product_detail_view, category_view
 
 urlpatterns = patterns('',
                         url(r'^detail/(?P<slug>[-\w]+)$', product_detail_view, name="catalogue_product"),
+                        url(r'^(?P<category_slug>[-\w]*)$', category_view, name="catalogue_category"),
 )
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,9 +1,17 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from catalogue.models import Product
+from catalogue.models import Product, Category
 
 
 def product_detail_view(request, slug=""):
     product = get_object_or_404(Product, slug=slug, is_active=True)
     return render_to_response("product_detail.html", locals(), context_instance=RequestContext(request))
+
+
+def category_view(request, category_slug=""):
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+    else:
+        category = None
+    return render_to_response("category.html", locals(), context_instance=RequestContext(request))
