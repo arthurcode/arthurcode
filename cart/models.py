cba897bc72f8635258d58eca0abe014bc5afe315
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-15 10:24:43 -0600

    models: create a 'CartItem' model
    A shopping cart (referenced by unique id) will have one or more cart
    items.

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from catalogue.models import Product
 
-# Create your models here.
+
+class CartItem(models.Model):
+    cart_id = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, unique=False)
+
+    class Meta:
+        ordering = ['date_added']
+
+    def total(self):
+        return self.quantity * self.product.price
+
+    def name(self):
+        return self.product.name
+
+    @property
+    def price(self):
+        return self.product.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url
+
+    def augment_quantity(self, quantity):
+        self.quantity += int(quantity)
+        self.save()
+
+    def __unicode__(self):
+        return self.product.name
