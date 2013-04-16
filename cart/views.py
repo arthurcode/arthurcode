676c8d9b6643d29a41e411a28dd7486c58dba735
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-16 13:50:49 -0600

    views: add a 'continue shopping' link to the cart page
    If the last url was a product/ url then link back to it, otherwise link
    to the main product(s) page.

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from cart import cartutils
+from django.core.urlresolvers import reverse
 
 
 def show_cart(request):
@@ -14,4 +15,10 @@ def show_cart(request):
             pass
     cart_items = cartutils.get_cart_items(request)
     cart_subtotal = cartutils.cart_subtotal(request)
+    continue_shopping_url = reverse('catalogue_category', kwargs={'category_slug': ''})
+    last_url = request.META.get('HTTP_REFERER', None)
+
+    if last_url and continue_shopping_url in last_url:
+        continue_shopping_url = last_url
+
     return render_to_response('cart.html', locals(), context_instance=RequestContext(request))
