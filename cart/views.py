68a609733222f228f3fdab716ddd6bae56c1bbff
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-17 08:59:17 -0600

    views: improve the continue-shopping url algorithm
    Make sure the continue_shopping url will never redirect a user off the
    site, even if the previous url happened to contain the /products
    fragment.  Also, I'm now preserving the get query parameters of the
    referrer url.

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from cart import cartutils
 from django.core.urlresolvers import reverse
+from urlparse import urlparse
 
 
 def show_cart(request):
@@ -18,7 +19,13 @@ def show_cart(request):
     continue_shopping_url = reverse('catalogue_category', kwargs={'category_slug': ''})
     last_url = request.META.get('HTTP_REFERER', None)
 
-    if last_url and continue_shopping_url in last_url:
-        continue_shopping_url = last_url
+    if last_url:
+        pr = urlparse(last_url)
+        path = pr[2]
+        if path.startswith(continue_shopping_url):
+            continue_shopping_url = path
+            query = pr[4]
+            if query:
+                continue_shopping_url += "?%s" % query
 
     return render_to_response('cart.html', locals(), context_instance=RequestContext(request))
