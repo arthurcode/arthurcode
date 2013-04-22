eb3240bd413563739abb9d02dee666107f319062
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-22 11:32:34 -0600

    models: validate that the user associated with a customer has a first and last name set

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from utils.models import AbstractAddress
+from utils.validators import is_blank
+from django.core.exceptions import ValidationError
 
 
 class CustomerProfile(models.Model):
@@ -21,6 +23,12 @@ class CustomerProfile(models.Model):
 
     def __unicode__(self):
         return "%s %s (%s)" % (self.first_name, self.last_name, self.user.username)
+
+    def clean(self):
+        # verify that the user model has a non-null first and last name field
+        if self.user_id and (is_blank(self.user.first_name) or is_blank(self.user.last_name)):
+            raise ValidationError(u"The associated user's first and/or last name cannot be blank")
+
 
 
 class CustomerShippingAddress(AbstractAddress):
