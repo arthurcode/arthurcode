5d3b37c1b0850cf343f54b3c1d5332ab66c189ae
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-21 21:16:41 -0600

    models: create an abstract Address model
    At this point I have no idea how well this model will serve the test of
    time, but it's a starting point, at least.

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from utils.validators import not_blank
 
-# Create your models here.
+
+class AbstractAddress(models.Model):
+    """
+    An abstract address class.  The name, first address line, and country fields are required.
+    Most addresses probably do require a postal code, but I believe there are some international ones that
+    don't.  Similarly the region may not always be required.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Addressee", validators=[not_blank])
+    line1 = models.CharField(max_length=200, verbose_name="Address Line 1", validators=[not_blank])
+    line2 = models.CharField(max_length=200, verbose_name="Address Line 2", null=True, blank=True)
+    country = models.CharField(max_length=50, validators=[not_blank])
+    post_code = models.CharField(max_length=20, verbose_name="Zip/Postal Code", null=True, blank=True)
+    region = models.CharField(max_length=50, verbose_name="Province/State", null=True, blank=True)
+
+    class Meta:
+        abstract = True
