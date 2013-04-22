e2fe50a89ae060c3d1c82bfbf560917af280a02c
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-22 11:25:16 -0600

    models: add __unicode__ for CustomerProfile

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,9 @@ class CustomerProfile(models.Model):
     def last_name(self):
         return self.user.last_name
 
+    def __unicode__(self):
+        return "%s %s (%s)" % (self.first_name, self.last_name, self.user.username)
+
 
 class CustomerShippingAddress(AbstractAddress):
     # a customer can have several shipping addresses associated with their profile
