4c6fa4d9f7d298970b021728b5d4641506074451
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-23 13:53:46 -0600

    templates: add an authentication form to the login_or_create_account template

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,10 @@ from arthurcode import settings
 from django.contrib.sites.models import get_current_site
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 @sensitive_post_parameters()
 @csrf_protect
@@ -21,30 +25,33 @@ def login_or_create_account(request,
     Displays the login form and handles the login action.  Largely copied from the built-in auth login/ view.
     """
     redirect_to = request.REQUEST.get(redirect_field_name, '')
+    auth_form = None
 
     if request.method == "POST":
-        form = authentication_form(data=request.POST)
-        if form.is_valid():
-            # Ensure the user-originating redirection url is safe.
-            if not is_safe_url(url=redirect_to, host=request.get_host()):
-                redirect_to = settings.LOGIN_REDIRECT_URL
+        postdata = request.POST.copy()
+        if u'login' in postdata:
+            auth_form = authentication_form(data=postdata)
+            if auth_form.is_valid():
+                # Ensure the user-originating redirection url is safe.
+                if not is_safe_url(url=redirect_to, host=request.get_host()):
+                    redirect_to = settings.LOGIN_REDIRECT_URL
 
-            # Okay, security check complete. Log the user in.
-            auth_login(request, form.get_user())
+                # Okay, security check complete. Log the user in.
+                auth_login(request, auth_form.get_user())
 
-            if request.session.test_cookie_worked():
-                request.session.delete_test_cookie()
+                if request.session.test_cookie_worked():
+                    request.session.delete_test_cookie()
 
-            return HttpResponseRedirect(redirect_to)
-    else:
-        form = authentication_form(request)
+                return HttpResponseRedirect(redirect_to)
+
+    auth_form = auth_form or authentication_form(request)
 
     request.session.set_test_cookie()
 
     current_site = get_current_site(request)
 
     context = {
-        'form': form,
+        'auth_form': auth_form,
         redirect_field_name: redirect_to,
         'site': current_site,
         'site_name': current_site.name,
@@ -54,9 +61,9 @@ def login_or_create_account(request,
     return TemplateResponse(request, template_name, context,
                             current_app=current_app)
 
-
+@login_required()
 def show_account(request):
     """
     Shows a user's account settings.  In this context the user is a customer
     """
-    pass
+    return render_to_response('my_account.html', locals(), context_instance=RequestContext(request))
