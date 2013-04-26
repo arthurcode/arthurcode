b2b6a7489e6b819c3d3b736c99fb99a9870974d8
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-26 14:12:49 -0600

    forms: add some local flavour to my CA shipping form

--- utils/forms.py
+++ utils/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from models import AbstractAddress
+from django.contrib.localflavor.ca.forms import CAPhoneNumberField, CAPostalCodeField, CAProvinceSelect, CAProvinceField
 
 
 class CanadaShippingForm(forms.Form):
     name = forms.CharField(max_length=AbstractAddress.ADDRESSEE_LENGTH, label="Recipient's Full Name")
-    phone = forms.CharField(max_length=AbstractAddress.PHONE_NUMBER_LENGTH, label="Recipient's Phone Number",
-                            help_text='This is required by some shipping companies.')
+    phone = CAPhoneNumberField(label="Recipient's Phone Number",
+                                     help_text='(Example: 403-555-7777) This is required by some shipping companies.')
     line1 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 1")
     line2 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 2", required=False)
     city = forms.CharField(max_length=AbstractAddress.CITY_LENGTH, label="City/Town")
-    province = forms.CharField(max_length=AbstractAddress.REGION_LENGTH, label="Province")
-    postal_code = forms.CharField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Postal Code")
+    province = CAProvinceField(widget=CAProvinceSelect)
+    postal_code = CAPostalCodeField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Postal Code",
+                                    help_text='(Example: T1B 2K9)')
