727b7792821dbdd979e922e9b2fa752e160c6838
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-04-26 12:16:48 -0600

    forms: add a 'retype email' field to my contact info form
    It's pretty darn important to get the email correct, so it's best to
    have a confirmation field.

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,6 +3,7 @@ from accounts.models import CustomerProfile
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
 
 class ContactInfoForm(forms.Form):
@@ -12,10 +13,18 @@ class ContactInfoForm(forms.Form):
     first_name = forms.CharField(max_length=30, required=True)  # 30 is the max length set by User
     last_name = forms.CharField(max_length=30, required=True)   # 30 is the max length set by User
     email = forms.EmailField(required=True)
+    email2 = forms.EmailField(required=True, label="Retype Email")
     contact_method = forms.ChoiceField(choices=CustomerProfile.CONTACT_METHOD, initial=CustomerProfile.EMAIL,
                                        widget=forms.RadioSelect,
                                        label="If there is a problem with your order how should we contact you?")
     phone = forms.CharField(max_length=20, required=False)
+
+    def clean_email2(self):
+        email1 = self.cleaned_data.get('email', None)
+        email2 = self.cleaned_data.get('email2', '')
+        if email1 and email2 and email1 != email2:
+            raise ValidationError(u"The two email addresses do not match.")
+        return email2
 
     def clean(self):
         cleaned_data = super(ContactInfoForm, self).clean()
