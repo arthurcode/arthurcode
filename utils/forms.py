9124e010796bae88d2f021baa987f7f253e46c5d
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-05-03 14:29:35 -0600

    forms: refactor my address forms to extend from a single base class.
    I'm forcing the forms to use the same field names, but I'm allowing them to customize the field itself.  This will make it much easier to create Address models from the forms later on.

--- utils/forms.py
+++ utils/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from models import AbstractAddress
 from django.contrib.localflavor.ca.forms import CAPhoneNumberField, CAPostalCodeField, CAProvinceSelect, CAProvinceField
 from django_countries import countries
+from utils.models import AbstractAddress
 
 
 # display as an optgroup with commonly selected countries at the top
@@ -12,27 +13,72 @@ COUNTRY_CHOICES = [
 ]
 
 
-class CanadaShippingForm(forms.Form):
-    name = forms.CharField(max_length=AbstractAddress.ADDRESSEE_LENGTH, label="Recipient's Full Name")
-    phone = CAPhoneNumberField(label="Recipient's Phone Number",
-                                     help_text='(Example: 403-555-7777) This is required by some shipping companies.')
-    line1 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 1")
-    line2 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 2", required=False)
-    city = forms.CharField(max_length=AbstractAddress.CITY_LENGTH, label="City/Town")
-    province = CAProvinceField(widget=CAProvinceSelect)
-    postal_code = CAPostalCodeField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Postal Code",
-                                    help_text='(Example: T1B 2K9)')
-
-
-class BillingForm(forms.Form):
+class AddressForm(forms.Form):
     """
     The DB requires that the name, address line1, and country fields be non-null.
     """
-    name = forms.CharField(max_length=AbstractAddress.ADDRESSEE_LENGTH, label="Name",
-                           help_text="Full name as it appears on your credit card.")
+    name = forms.CharField(max_length=AbstractAddress.ADDRESSEE_LENGTH, label="Name")
+    phone = forms.CharField(max_length=AbstractAddress.PHONE_NUMBER_LENGTH, label="Phone", required=False)
     line1 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 1")
     line2 = forms.CharField(max_length=AbstractAddress.LINE_LENGTH, label="Address Line 2", required=False)
     city = forms.CharField(max_length=AbstractAddress.CITY_LENGTH, label="City/Town")
     region = forms.CharField(max_length=AbstractAddress.REGION_LENGTH, label="State/Province")
-    postal_code = forms.CharField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Zip/Postal Code")
+    post_code = forms.CharField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Zip/Postal Code")
     country = forms.ChoiceField(choices=COUNTRY_CHOICES, label="Country")
+
+    def __init__(self, *args, **kwargs):
+        super(AddressForm, self).__init__(*args, **kwargs)
+        for field in self.fields.keys():
+            customize_method = 'customize_' + field
+            if hasattr(self, customize_method):
+                customize = getattr(self, customize_method)
+                customize(self.fields[field])
+
+    def save(self, clazz, commit=True):
+        cd = self.cleaned_data
+        if not issubclass(clazz, AbstractAddress):
+            raise Exception("The AddressForm meta class is not a subclass of " + AbstractAddress.__class__.__name___)
+        address = clazz()
+        address.name = cd['name']
+        address.phone = cd['phone']
+        address.line1 = cd['line1']
+        address.line2 = cd['line2']
+        address.city = cd['city']
+        address.region = cd['region']
+        address.post_code = cd['post_code']
+        address.country = cd['country']
+        if commit:
+            address.save()
+        return address
+
+
+class CanadaShippingForm(AddressForm):
+
+    def customize_name(self, field):
+        field.label = "Recipient's Full Name"
+
+    def customize_phone(self, field):
+        self.fields['phone'] = CAPhoneNumberField(label="Recipient's Phone Number", required=True,
+                                                  help_text="This is required by some shipping companies.")
+
+    def customize_region(self, field):
+        self.fields['region'] = CAProvinceField(widget=CAProvinceSelect, label="Province")
+
+    def customize_post_code(self, field):
+        self.fields['post_code'] = CAPostalCodeField(max_length=AbstractAddress.POST_CODE_LENGTH, label="Postal Code",
+                                     help_text='(Example: T1B 2K9)')
+
+    def customize_country(self, field):
+        field.required = False
+
+    def save(self, clazz, commit=True):
+        address = super(CanadaShippingForm, self).save(clazz, commit=False)
+        address.country = "CA"
+        if commit:
+            address.save()
+        return address
+
+
+class BillingForm(AddressForm):
+    def customize_name(self, field):
+        field.help_text = "Full name as it appears on your credit card."
