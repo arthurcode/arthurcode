652d02e859d1a2433a3e396d7ee747acee487a35
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-06-10 14:57:55 -0600

    search: include category matches in our search results
    If a category 'hits' on a particular set of search terms then include products that fall within that category, regardless of whether or not the product itself contains all the relevant search terms.

--- search/searchutils.py
+++ search/searchutils.py
@@ -1,5 +1,5 @@
 from search.models import SearchTerm
-from catalogue.models import Product
+from catalogue.models import Product, Category
 from django.db.models import Q
 
 STRIP_WORDS = ['a', 'an', 'and', 'by', 'for', 'from', 'in', 'no', 'not', 'of', 'on', 'or', 'that', 'the', 'to', 'with']
@@ -19,21 +19,47 @@ def store(request, q):
         term.save()
 
 
+def categories(search_text, categories=None):
+    words = _prepare_words(search_text)
+    return _categories(words, categories)
+
+
+def _categories(search_words, categories=None):
+    if not categories:
+        categories = Category.objects.filter(is_active=True)
+    for word in search_words:
+        categories = categories.filter(Q(name__icontains=word) |
+                                        Q(description__icontains=word))
+    return categories
+
+
 def products(search_text, products=Product.active.all()):
     """
     Search for products matching the given search text.  By default all active products are queried, but this can be
     modified by passing a custom 'products' queryset.
     """
     words = _prepare_words(search_text)
+    product_query = Q()
 
+    # for a product to match every word needs to appear in at least one of the following fields
     for word in words:
-        products = products.filter(Q(name__icontains=word) |
-                                    Q(short_description__icontains=word) |
-                                    Q(long_description__icontains=word) |
-                                    Q(upc__iexact=word) |                   # TODO include sku when I have a sku
-                                    Q(brand__name__icontains=word) |
-                                    Q(themes__name__icontains=word))
-    return products
+        product_query &= (Q(name__icontains=word) |
+                          Q(short_description__icontains=word) |
+                          Q(long_description__icontains=word) |
+                          Q(upc__iexact=word) |                   # TODO include sku when I have a sku
+                          Q(brand__name__icontains=word) |
+                          Q(themes__name__icontains=word))
+
+    # factor in category matches
+    categories = _categories(words)
+    category_query = Q()
+
+    # to get a category match the product needs to appear in at least one of the matching categories
+    for category in categories:
+        category_query |= Q(category__in=category.get_leafnodes(include_self=True))
+
+    # return products that either match on the product query or the catalogue query
+    return products.filter(Q(product_query | category_query)).distinct()
 
 
 def _prepare_words(search_text):
