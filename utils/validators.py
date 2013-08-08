2160a075a1c5d574aa4998e629ebd4c135ecfe22
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-08-08 11:16:06 -0600

    validators: add a SKU validator
    I have come to the realization that it is SKU and not UPC that I should be tracking in my product models.  Prepare to make the switch.

--- utils/validators.py
+++ utils/validators.py
@@ -5,8 +5,10 @@ import re
 
 ERROR_BLANK = "This field cannot be blank."  # same as built-in 'blank' error
 ERROR_UPC = "The UPC must consist of exactly 12 digits."
+ERROR_SKU = "The SKU must consist of 3-10 alphanumeric characters, cannot start with a 0, and may not contain the letters I, L or O."
 
 UPC_PATTERN = re.compile('^\d{12}$')         # 12 digits
+SKU_PATTERN = re.compile('^[1-9A-HJKMNP-Z][0-9A-HJKMNP-Z]{2,9}$')
 
 
 def not_blank(value):
@@ -25,6 +27,18 @@ def valid_upc(upc_str):
     """
     if not upc_str or not UPC_PATTERN.match(upc_str):
         raise ValidationError(ERROR_UPC)
+
+
+def valid_sku(sku_str):
+    """
+    Tests that the given sku is valid:
+      * must be between 3 and 10 alphanumeric characters in length (uppercase letters only)
+      * cannot start with a 0
+      * must not contain the letters (O, I, L) because they can be confused with numbers
+      see http://www.clearlyinventory.com/inventory-basics/how-to-design-good-item-numbers-for-products-in-inventory
+    """
+    if not sku_str or not SKU_PATTERN.match(sku_str):
+        raise ValidationError(ERROR_SKU)
 
 
 # utils
