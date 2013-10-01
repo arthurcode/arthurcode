79ab38a6f1a1ce9311d19aa1cdeee5a9e8d358a4
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-10-01 10:24:01 -0600

    views: preserve url params when we get a search error

--- search/views.py
+++ search/views.py
@@ -10,13 +10,13 @@ import urllib
 def product_search_view(request):
     post_data = request.POST.copy()
     form = SearchForm(data=post_data)
+    params = request.GET.copy()
     if form.is_valid():
         # save the search query in the database
         q = form.cleaned_data['q']
         category_slug = form.cleaned_data.get('category_slug', '')
         searchutils.store(request, q)
         url = reverse('catalogue_category', kwargs={'category_slug': category_slug})
-        params = request.GET.copy()
         params['search'] = q
         return HttpResponseRedirect(url + "?" + urllib.urlencode(params))
-    return HttpResponseRedirect(post_data['from'])
+    return HttpResponseRedirect(post_data['from'] + "?" + urllib.urlencode(params))
