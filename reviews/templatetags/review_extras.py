e96383bb6ef6be9d47acf27128c29d96b2a3481c
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-10-04 13:49:09 -0600

    template tags: re-use code in the review_thumb tag

--- reviews/templatetags/review_extras.py
+++ reviews/templatetags/review_extras.py
@@ -1,10 +1,8 @@
 from django.template import Library
+from catalogue.templatetags.catalogue_extras import thumb
 
 register = Library()
 
 @register.inclusion_tag('_review_it_thumbnail.html', takes_context=True)
 def review_thumb(context, product):
-    return {
-        'product': product,
-        'request': context.get('request', None)
-    }
+    return thumb(context, product)
