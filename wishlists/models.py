edf3c708fe3f568cc4f81fbd5186eb8f9618ade3
Rhyan Arthur <rhyan.arthur@gmail.com>
2013-10-11 15:14:51 -0600

    models: remove the 'rank' field from wish list items
    I think I'm over-complicating wish lists.  If a 'rank' and a way to modify rank turns out to be a useful feature I can add it in later.

--- wishlists/models.py
+++ wishlists/models.py
@@ -22,9 +22,7 @@ class WishList(models.Model):
         return reverse('wishlist_view', args=[self.id])
 
     def add_product(self, product_instance, note=None):
-        max_rank = self.items.all().aggregate(Max('rank')).get('rank__max', None) or 0
-        rank = max_rank + 1
-        item = WishListItem(wish_list=self, instance=product_instance, note=note, rank=rank)
+        item = WishListItem(wish_list=self, instance=product_instance, note=note)
         item.full_clean()
         item.save()
         return item
@@ -37,4 +35,3 @@ class WishListItem(models.Model):
     instance = models.ForeignKey(ProductInstance)
     note = models.CharField(max_length=NOTE_MAX_LENGTH, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
-    rank = models.PositiveIntegerField(help_text="The rank of this item relative to the others in the list")
