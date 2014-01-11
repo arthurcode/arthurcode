9de6caf9ccd0868c36c73de5c63b9906ecc78a98
Rhyan Arthur <rhyan.arthur@gmail.com>
2014-01-10 19:33:46 -0700

    mailing-list: add unsubscribe links
    Add a mailing-list/unsubscribe view for logged in users.  For users that aren't logged in provide a view that accepts a cryptographically signed email address.  The email that we send out will have an 'unsubscribe' link at the bottom that directs to this view.

--- emaillist/urls.py
+++ emaillist/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
-from emaillist.views import subscribe, subscribe_complete
+from emaillist.views import subscribe, subscribe_complete, unsubscribe, unsubscribe_user, unsubscribe_complete
 
 urlpatterns = patterns('',
                        url(r'^subscribe/$', subscribe, name="subscribe"),
                        url(r'^subscribe-complete/$', subscribe_complete, name="subscribe_complete"),
+                       url(r'^unsubscribe$', unsubscribe_user, name="unsubscribe_user"),
+                       url(r'^unsubscribe/(\S+)$', unsubscribe, name="unsubscribe"),
+                       url(r'^unsubscribe-complete/$', unsubscribe_complete, name="unsubscribe_complete"),
                        )
--- emaillist/views.py
+++ emaillist/views.py
@@ -2,7 +2,10 @@ from forms import SubscribeToMailingListForm
 from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.template import RequestContext
 from django.views.decorators.http import require_GET
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.core.signing import Signer
+import utils
 
 def subscribe(request):
     if request.method == "POST":
@@ -23,4 +26,36 @@ def subscribe(request):
 @require_GET
 def subscribe_complete(request):
     return render_to_response('subscribe_complete.html', {}, context_instance=RequestContext(request))
+
+@login_required
+@require_GET
+def unsubscribe_user(request):
+    """
+    An unsubscribe view that is available to logged in users.
+    """
+    signer = Signer()
+    signed_email = signer.sign(request.user.email)
+    return HttpResponseRedirect(reverse('unsubscribe', args=[signed_email]))
+
+
+def unsubscribe(request, email_token):
+    email = Signer().unsign(email_token)
+
+    if request.method == "POST":
+        data = request.POST.copy()
+        if 'cancel' in data:
+            # TODO: maybe pass a next link along to this view?
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            utils.remove_from_list(email)
+            return HttpResponseRedirect(reverse('unsubscribe_complete'))
+
+    context = {
+        'email': email,
+    }
+    return render_to_response('unsubscribe.html', context, context_instance=RequestContext(request))
+
+
+def unsubscribe_complete(request):
+    return render_to_response('unsubscribe_complete.html', {}, context_instance=RequestContext(request))
 
